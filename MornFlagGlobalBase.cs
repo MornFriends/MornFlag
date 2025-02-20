ae83a4a510dc4c8830415e005b5b46f68d06a0c0
matsufriends <pikapikan0526@gmail.com>
2025-02-21 01:49:47 +0900

    Flagsをpublicに

--- MornFlagGlobalBase.cs
+++ MornFlagGlobalBase.cs
@@ -7,6 +7,6 @@ namespace MornFlag
     public abstract class MornFlagGlobalBase : MornGlobalBase<MornFlagGlobalBase>
     {
         [SerializeField] private List<string> _flags = new();
-        internal List<string> Flags => _flags;
+        public List<string> Flags => _flags;
     }
 }
